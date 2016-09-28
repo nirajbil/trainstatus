6d1f8118d987319ef71bd19933639c7ac24a6cf3
niraj bilaimare <nirajkb@vedams.com>
2016-09-28 08:57:26 +0530

    Add requirement

---
+++ railway/settings/production.py
@@ -0,0 +1,31 @@
+import os
+from django.conf import settings
+
+
+DEBUG = False
+TEMPLATE_DEBUG = True
+
+DATABASES = settings.DATABASES
+
+# parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+#Honor  the "X-FORWARDED-Proto" header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO','https')
+
+#allow all host header
+ALLOWED_HOSTS = ['*']
+
+#static asset configuration
+#import os
+#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+#STATIC_ROOT = "staticfiles"
+#STATIC_URL = "/static/"
+
+#STATICFILES_DIRS = {
+#    os.path.join(BASE_DIR,'static'),
+#}
+
+
+
